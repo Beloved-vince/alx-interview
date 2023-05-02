1dbec6997cc155bf1fdcac3452d5b5c9a8293e5f
Beloved-vince <vinceoludare@email.com>
2023-05-02 17:02:32 +0100

    lockboxes

---
+++ 0x02-minimum_operations0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Minimum operations coding challenge.
+    Prototype: def minOperations(n)
+    
+    Example:
+    n = 9
+    H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+    Number of operations: 6
+"""
+
+def minOperations(n):
+    """
+         method that calculates the fewest number of operations needed
+         to result in exactly n H characters in the file.
+    """
+    # If n is impossible to achieve, return 0
+    if not isinstance(n, int):
+        return 0
+    # Returns an integer
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return 
