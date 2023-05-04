31d8e439749427b94afab1dda09215651fcc30de
Beloved-vince <vinceoludare@email.com>
2023-05-04 22:47:29 +0100

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,30 +8,34 @@
     H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
     Number of operations: 6
 """
-
 def minOperations(n):
     """
          method that calculates the fewest number of operations needed
          to result in exactly n H characters in the file.
     """
-    # If n is impossible to achieve, return 0
     if not isinstance(n, int):
         return 0
-    # Returns an integer
     ops_count = 0
     clipboard = 0
     done = 1
-    
+    # print('H', end='')
     while done < n:
         if clipboard == 0:
+            # init (the first copy all and paste)
             clipboard = done
             done += clipboard
             ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
         elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
             clipboard = done
             done += clipboard
             ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
         elif clipboard > 0:
+            # paste
             done += clipboard
             ops_count += 1
-    return 
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
